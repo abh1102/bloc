5d65d7b1978ce1f13c330177ccd18134db7f5f3b
abh1102 <abhishekjha2507@gmail.com>
2025-01-10 00:11:06 +0530

    commmot

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,6 +1,8 @@
 import 'package:bloc_project_todo/core/routes/route_name.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../features/splash/view/splashview.dart';
+
 final GoRouter router=GoRouter(routes: [
   GoRoute(name: RouteNames.splash,path: "/",builder: (context,state)=>SplashView(),)
 ]);
---
+++ lib/core/utils/app_image_url.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+class AppImageUrl{
+static const logo='assets/images/algo.jpg';
+}
